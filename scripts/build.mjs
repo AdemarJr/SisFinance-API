d2ea6fd7d56ec6d6f83928ba090ef49dc6acecc1
ademarfarias <ademarfariasjunior@gmail.com>
2026-07-06 08:50:28 -0400

    ajustando para node 22

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -10,7 +10,7 @@ await build({
   entryPoints: ['src/index.ts'],
   bundle: true,
   platform: 'node',
-  target: 'node18',
+  target: 'node22',
   format: 'esm',
   outfile: 'dist/index.js',
   logLevel: 'info',
